85b401eb162a3ae84ece94cb19c1b0c176c497b3
Brian Jenney <brianjenney83@gmail.com>
2016-07-18 17:49:22 -0700

    Plus one function added to buttons

--- controllers/SubmitController.js
+++ controllers/SubmitController.js
@@ -12,18 +12,24 @@ angular.module('myApp.submit', ['ngRoute','firebase'])
 
 	var url = "https://questionvote.firebaseio.com/Questions"
 
+	//function to submit questions
 	$scope.submitQuestion = function(){
 		var fb = new Firebase(url)
 		fb.push({
 			question: $scope.question.text, 
 			parameter1: $scope.question.parameterOne,
-			parameter2: $scope.question.parameterTwo
+			parameter2: $scope.question.parameterTwo,
+			vote1: 0,
+			vote2: 0,
+			voters:[]
 		})
 
+		//set input values back to blank
 		$scope.question.text = "";
 		$scope.question.parameterOne = "";
 		$scope.question.parameterTwo ="";
 
+		//direct user to vote page
 		$location.path('/vote');
 
 	}
--- controllers/VoteController.js
+++ controllers/VoteController.js
@@ -7,13 +7,59 @@ angular.module('myApp.vote', ['ngRoute'])
   });
 }])
 
-.controller('VoteCtrl', ['$scope', '$firebase', '$firebaseArray', function($scope, $firebase, $firebaseArray) {
+.controller('VoteCtrl', ['$scope', '$firebase', '$firebaseArray', '$firebaseObject', function($scope, $firebase, $firebaseArray, $firebaseObject) {
  
- 	var fb = new Firebase("https://questionvote.firebaseio.com/Questions");
+ 	var fb = new Firebase("https://questionvote.firebaseio.com/Questions/");
+
 
  	$scope.question = $firebaseArray(fb);
 
- 	console.log($scope.question);
+ 	//vote for side one function
+ 	$scope.voteSideOne = function(id){
+ 		
+ 		//create array from fb
+ 		var fb = $firebaseArray(new Firebase("https://questionvote.firebaseio.com/Questions/"+id));
+ 		 var vote = new Firebase("https://questionvote.firebaseio.com/Questions/"+id);
+        
+ 		//store the user array as an object to add one 
+ 		//to the votes
+ 		//needs functionality to make sure same user can't vote more than once for the same question 
+ 		fb.$loaded().then(function(){
+ 			//get value of votes for param 1
+ 			var newVoteTotal = fb[3].$value + 1;
+ 			//update db with new vote total
+ 			console.log(newVoteTotal)
+
+ 			vote.update({
+ 				vote1: newVoteTotal
+ 			})
+ 			
+ 		});
+ 		
+ 		
+ 	}
+
+ 	$scope.voteSideTwo = function(id){
+ 		//create array from fb
+ 		var fb = $firebaseArray(new Firebase("https://questionvote.firebaseio.com/Questions/"+id));
+ 		 var vote = new Firebase("https://questionvote.firebaseio.com/Questions/"+id);
+        
+ 		//store the user array as an object to add one 
+ 		//to the votes
+ 		//needs functionality to make sure same user can't vote more than once for the same question 
+ 		fb.$loaded().then(function(){
+ 			//get value of votes for param 1
+ 			var newVoteTotal = fb[4].$value + 1;
+ 			//update db with new vote total
+ 			console.log(newVoteTotal)
+
+ 			vote.update({
+ 				vote2: newVoteTotal
+ 			})
+ 			
+ 		});
+
+ 	}
 
 
  }]);
